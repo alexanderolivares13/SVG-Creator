6ae9d6bd0ae39eb45322417725af154604111cdd
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-07-17 21:10:15 -0700

    added comments, fixed some sizing with the SVG syntax, and removed some unused code.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+//only importing the shape handler, as it uses the user data to create and return the SVG syntax.
 const { shapeHandler } = require("./lib/shapes");
+
+// all the questions that will be used by inquirer, I added in some break points at the end of the questions for readability.
 const questions = [
   {
     message:
@@ -20,11 +23,13 @@ const questions = [
     name: "color",
   },
   {
-    message: "Would you like your SVG to have an outline?\n",
+    // this question returns a boolean value as it will ask a 2nd follow up question if the user answers yes.
+    message: "Would you like your SVG shape to have an outline?\n",
     type: "confirm",
     name: "stroke",
   },
   {
+    // the follow up question will only be asked if the user answers yes to the previous question.
     message:
       "What color would you like the outline to be?\n(You can use any color keyword or any Hexadecimal value including the #)\n",
     type: "input",
@@ -33,6 +38,7 @@ const questions = [
     when: (response) => response.stroke === true,
   },
   {
+    // the validate property here inserts a function that doesn't let the user input more than 3 characters.
     message: "What 3 characters would you like your SVG to display?\n",
     type: "input",
     name: "text",
@@ -52,12 +58,14 @@ const questions = [
   },
 ];
 
+// this function creates the svg file to the example folder by calling the shapeHandler function that is imported from the shapes.js file.
 function writeToFile(data) {
   fs.writeFile(`./examples/logo.svg`, shapeHandler(data), (err) =>
     err ? console.log(err) : console.info("Generated logo.svg")
   );
 }
 
+// the function init calls the inquirer prompt then passes in the questions object arra, and returns the data to be used to write the SVG file.
 function init() {
   inquirer
   .prompt(questions)
@@ -66,4 +74,5 @@ function init() {
   });
 }
 
+// invoking the init function. begin the question prompt by opening bash/terminal and typing ``` node index.js ```
 init();
